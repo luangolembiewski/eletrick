a0862b9e225da4f8b6f3c04a3629bf3f198c962d
Luan Golembiewski <luangolembiewski74@gmail.com>
2023-05-07 18:51:39 -0300

    Att

--- src/models/connection.js
+++ src/models/connection.js
@@ -3,10 +3,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const connection = mysql.createPool({
-    host: 6128,
-    user: root,
-    password: De0opgarg8NU0mNeQq1j, 
-    database: railway
+    host: '6128',
+    user: 'root',
+    password: 'De0opgarg8NU0mNeQq1j', 
+    database: 'railway'
 });
 
 module.exports = connection;
